4c8024776855a1e1af5e244eefc927de460ce483
Khalid Zerri <kzerri@e3r3p2.1337.ma>
2023-12-22 18:42:20 +0100

    lb

--- cub3d.c
+++ cub3d.c
@@ -6,27 +6,38 @@
 /*   By: kzerri <kzerri@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:11:30 by kzerri            #+#    #+#             */
-/*   Updated: 2023/12/22 17:43:48 by kzerri           ###   ########.fr       */
+/*   Updated: 2023/12/22 18:40:16 by kzerri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void l()
+{
+	system("leaks cub3D");
+}
+
 int main(int ac, char **av)
 {
 	t_mlx		mlx;
 	t_player	player;
-	t_textures	*text;
+	t_textures	*texture;
+	t_paths		*text_paths;
 
+	// atexit(l);
 	if (ac != 2)
 		return (ft_putstr("Error\n"), 1);
 	if (parse_arg(av[1]))
 		return (1);
-	text = NULL;
-	text = malloc(sizeof(t_textures));
-	glob_pars(av, text);
-	player.text = text;
+	texture = malloc(sizeof(t_textures));
+	memset(texture, 0, sizeof(t_textures));
+	text_paths = malloc(sizeof(t_paths));
+	memset(text_paths, 0, sizeof(t_paths));
+	glob_pars(av, texture);
+	load_png(text_paths, texture);
+	player.text = texture;
+	player.paths = text_paths;
 	set_up_window(&mlx, &player);
 	keep_window_opened(&mlx, &player);
-	free_map_text(text);
+	free_map_text(texture);
 }
