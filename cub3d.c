524f128ecb4747796162599ab55c1cfc0037d271
Anas Raji Afoua <araji-af@e3r4p3.1337.ma>
2023-12-22 03:39:32 +0100

    cc

--- cub3d.c
+++ cub3d.c
@@ -6,25 +6,18 @@
 /*   By: kzerri <kzerri@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:11:30 by kzerri            #+#    #+#             */
-/*   Updated: 2023/12/22 17:28:57 by kzerri           ###   ########.fr       */
+/*   Updated: 2023/12/22 17:43:48 by kzerri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void l()
-{
-	system("leaks cub3D");
-}
-
 int main(int ac, char **av)
 {
 	t_mlx		mlx;
 	t_player	player;
-	t_textures	*texture;
-	t_paths		*text_paths;
+	t_textures	*text;
 
-	// atexit(l);
 	if (ac != 2)
 		return (ft_putstr("Error\n"), 1);
 	if (parse_arg(av[1]))
@@ -35,5 +28,5 @@ int main(int ac, char **av)
 	player.text = text;
 	set_up_window(&mlx, &player);
 	keep_window_opened(&mlx, &player);
-	free_map_text(texture);
+	free_map_text(text);
 }
